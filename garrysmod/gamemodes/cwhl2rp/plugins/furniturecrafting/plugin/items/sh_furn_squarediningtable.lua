07f6ae12f924fd334c98ae886a8d8c804909153b
The Lion <visiongamingjake@hotmail.com>
2019-03-11 22:26:30 +0100

    Uploads v0.2 of the furniturecraft plugin.
    Changes file names to be per category, and contains several useful updates, such as reduced output from crafting and storage capabilities for the furniture that should realistically be able to hold things.

--- garrysmod/gamemodes/cwhl2rp/plugins/furniturecrafting/plugin/items/sh_furn_squarediningtable.lua
+++ garrysmod/gamemodes/cwhl2rp/plugins/furniturecrafting/plugin/items/sh_furn_squarediningtable.lua
@@ -6,8 +6,9 @@ ITEM.model = "models/props_c17/FurnitureTable002a.mdl";
 ITEM.plural = "Square Dining Tables";
 ITEM.weight = 4;
 ITEM.category = "Furniture";
+ITEM.batch = 1;
+ITEM.useText = "Open";
 ITEM.description = "A square dining table. The quality is piss poor, but it looks like it should hold your food up just fine.";
-ITEM:AddRecipe("toolboxsmall", 1, "largeply", 1, "woodtiny", 4, "steelnails", 1);
 
 -- Called when a player drops the item.
 function ITEM:OnDrop(player, position) end;
