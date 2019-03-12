2b6f2ffcfa942bd6fd3cae2ce2787710a64a249a
The Lion <visiongamingjake@hotmail.com>
2019-03-13 00:41:24 +0100

    Updates MPF.

--- garrysmod/gamemodes/cwhl2rp/plugins/mpfullcustomization/plugin/sh_plugin.lua
+++ garrysmod/gamemodes/cwhl2rp/plugins/mpfullcustomization/plugin/sh_plugin.lua
@@ -22,17 +22,21 @@ otaprefix = "MTA"
 -- Set divisions. You can add and subtract divisions as you need
 divisions = {}
 	divisions[1] = "UNION"
-	divisions[2] = "GRID"
-	divisions[3] = "HELIX"
-	divisions[4] = "JUDGE"
+	divisions[2] = "SHIELD"
+	divisions[3] = "JUDGE"
 
 -- Set ranks. You can add and subtract ranks as you need
 ranks = {}
-ranks["i5"] = "i5"
-ranks["i4"] = "i4"
-ranks["i3"] = "i3"
-ranks["i2"] = "i2"
-ranks["i1"] = "i1"
+ranks["i5"] = "I"
+ranks["m5"] = "M:I"
+ranks["i4"] = "II"
+ranks["m4"] = "M:II"
+ranks["i3"] = "III"
+ranks["m3"] = "M:III"
+ranks["i2"] = "IV"
+ranks["m2"] = "M:IV"
+ranks["i1"] = "V"
+ranks["m1"] = "M:V"
 ranks["officer"] = "OfC"
 ranks["divisionleader"] = "DvL" // PLEASE leave the "divisionleader" key the same. You can change "DvL" to something else
 
@@ -40,33 +44,43 @@ ranks["divisionleader"] = "DvL" // PLEASE leave the "divisionleader" key the sam
 models = {}
 -- Start of Division 1
 models[divisions[1]] = {}
-models[divisions[1]][ranks["i5"]] = "models/dpfilms/metropolice/hdpolice.mdl"
-models[divisions[1]][ranks["i4"]] = "models/dpfilms/metropolice/hdpolice.mdl"
-models[divisions[1]][ranks["i3"]] = "models/dpfilms/metropolice/hdpolice.mdl"
-models[divisions[1]][ranks["i2"]] = "models/dpfilms/metropolice/retrocop.mdl"
-models[divisions[1]][ranks["i1"]] = "models/dpfilms/metropolice/retrocop.mdl"
-models[divisions[1]][ranks["officer"]] = "models/dpfilms/metropolice/elite_police.mdl"
-models[divisions[1]][ranks["divisionleader"]] = "models/dpfilms/metropolice/rtb_police.mdl"
+models[divisions[1]][ranks["i5"]] = "models/dpfilms/metropolice/c08cop.mdl"
+models[divisions[1]][ranks["m5"]] = "models/dpfilms/metropolice/civil_medic.mdl"
+models[divisions[1]][ranks["i4"]] = "models/dpfilms/metropolice/c08cop.mdl"
+models[divisions[1]][ranks["m4"]] = "models/dpfilms/metropolice/civil_medic.mdl"
+models[divisions[1]][ranks["i3"]] = "models/dpfilms/metropolice/c08cop.mdl"
+models[divisions[1]][ranks["m3"]] = "models/dpfilms/metropolice/civil_medic.mdl"
+models[divisions[1]][ranks["i2"]] = "models/dpfilms/metropolice/c08cop.mdl"
+models[divisions[1]][ranks["m2"]] = "models/dpfilms/metropolice/civil_medic.mdl"
+models[divisions[1]][ranks["i1"]] = "models/dpfilms/metropolice/policetrench.mdl"
+models[divisions[1]][ranks["m1"]] = "models/leet_police3_medic.mdl"
+models[divisions[1]][ranks["officer"]] = "models/leet_police3.mdl"
+models[divisions[1]][ranks["divisionleader"]] = "models/dpfilms/metropolice/phoenix_police.mdl"
 
 -- Start of Division 2
 models[divisions[2]] = {}
-models[divisions[2]][ranks["i5"]] = "models/dpfilms/metropolice/hdpolice.mdl"
-models[divisions[2]][ranks["i4"]] = "models/dpfilms/metropolice/hdpolice.mdl"
-models[divisions[2]][ranks["i3"]] = "models/dpfilms/metropolice/hdpolice.mdl"
-models[divisions[2]][ranks["i2"]] = "models/dpfilms/metropolice/retrocop.mdl"
-models[divisions[2]][ranks["i1"]] = "models/dpfilms/metropolice/retrocop.mdl"
-models[divisions[2]][ranks["officer"]] = "models/dpfilms/metropolice/elite_police.mdl"
-models[divisions[2]][ranks["divisionleader"]] = "models/dpfilms/metropolice/rtb_police.mdl"
+models[divisions[2]][ranks["i5"]] = "models/dpfilms/metropolice/urban_police.mdl"
+models[divisions[2]][ranks["m5"]] = "models/dpfilms/metropolice/civil_medic.mdl"
+models[divisions[2]][ranks["i4"]] = "models/dpfilms/metropolice/urban_police.mdl"
+models[divisions[2]][ranks["m4"]] = "models/dpfilms/metropolice/civil_medic.mdl"
+models[divisions[2]][ranks["i3"]] = "models/dpfilms/metropolice/urban_police.mdl"
+models[divisions[2]][ranks["m3"]] = "models/dpfilms/metropolice/civil_medic.mdl"
+models[divisions[2]][ranks["i2"]] = "models/dpfilms/metropolice/urban_police.mdl"
+models[divisions[2]][ranks["m2"]] = "models/dpfilms/metropolice/civil_medic.mdl"
+models[divisions[2]][ranks["i1"]] = "models/dpfilms/metropolice/policetrench.mdl"
+models[divisions[2]][ranks["m1"]] = "models/leet_police4_medic.mdl"
+models[divisions[2]][ranks["officer"]] = "models/leet_police4.mdl"
+models[divisions[2]][ranks["divisionleader"]] = "models/metrold_nobp.mdl"
 
 -- Start of Division 3
 models[divisions[3]] = {}
-models[divisions[3]][ranks["i5"]] = "models/dpfilms/metropolice/hdpolice.mdl"
-models[divisions[3]][ranks["i4"]] = "models/dpfilms/metropolice/hdpolice.mdl"
-models[divisions[3]][ranks["i3"]] = "models/dpfilms/metropolice/hdpolice.mdl"
-models[divisions[3]][ranks["i2"]] = "models/dpfilms/metropolice/retrocop.mdl"
-models[divisions[3]][ranks["i1"]] = "models/dpfilms/metropolice/retrocop.mdl"
-models[divisions[3]][ranks["officer"]] = "models/dpfilms/metropolice/elite_police.mdl"
-models[divisions[3]][ranks["divisionleader"]] = "models/dpfilms/metropolice/rtb_police.mdl"
+models[divisions[3]][ranks["i5"]] = "models/dpfilms/metropolice/arctic_police.mdl"
+models[divisions[3]][ranks["i4"]] = "models/dpfilms/metropolice/arctic_police.mdl"
+models[divisions[3]][ranks["i3"]] = "models/dpfilms/metropolice/arctic_police.mdl"
+models[divisions[3]][ranks["i2"]] = "models/dpfilms/metropolice/arctic_police.mdl"
+models[divisions[3]][ranks["i1"]] = "models/dpfilms/metropolice/arctic_police.mdl"
+models[divisions[3]][ranks["officer"]] = "models/leet_police5.mdl"
+models[divisions[3]][ranks["divisionleader"]] = "models/dpfilms/metropolice/blacop.mdl"
 
 // Feel free to add more divisions!
 
@@ -79,7 +93,7 @@ models[divisions[3]][ranks["divisionleader"]] = "models/dpfilms/metropolice/rtb_
 // This would be okay: nranks["eow"] = "EPIC"
 // This would NOT be okay: nranks["eowwww"] = "EOW"
 nranks = {}
-nranks["recruit"] = "RCT"
+nranks["recruit"] = "INT"
 nranks["eow"] = "EOW"
 nranks["ows"] = "OWS"
 nranks["echo"] = "ECHO"
