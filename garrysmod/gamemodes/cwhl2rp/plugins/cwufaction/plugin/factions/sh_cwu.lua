1c1e3b6e5b094a67610f1df879933015d04e1a32
The Lion <visiongamingjake@hotmail.com>
2019-03-14 22:28:40 +0100

    Updates CWU models.

--- garrysmod/gamemodes/cwhl2rp/plugins/cwufaction/plugin/factions/sh_cwu.lua
+++ garrysmod/gamemodes/cwhl2rp/plugins/cwufaction/plugin/factions/sh_cwu.lua
@@ -11,8 +11,37 @@ FACTION.useFullName = true;
 -- Calls models for character creation. Used sparingly since it's preferable to transfer.
 
 FACTION.models = {
-	female = {"placeholder1.mdl"},
-	male = {"placeholder1.mdl"}
+	female = {"models/tnb/citizens/female_01.mdl",
+	"models/tnb/citizens/female_02.mdl",
+	"models/tnb/citizens/female_03.mdl",
+	"models/tnb/citizens/female_04.mdl",
+	"models/tnb/citizens/female_05.mdl",
+	"models/tnb/citizens/female_06.mdl",
+	"models/tnb/citizens/female_07.mdl",
+	"models/tnb/citizens/female_08.mdl",
+	"models/tnb/citizens/female_09.mdl",
+	"models/tnb/citizens/female_10.mdl",
+	"models/tnb/citizens/female_11.mdl"
+	},
+	male = {"models/tnb/citizens/male_01.mdl",
+	"models/tnb/citizens/male_02.mdl",
+	"models/tnb/citizens/male_03.mdl",
+	"models/tnb/citizens/male_04.mdl",
+	"models/tnb/citizens/male_05.mdl",
+	"models/tnb/citizens/male_06.mdl",
+	"models/tnb/citizens/male_07.mdl",
+	"models/tnb/citizens/male_08.mdl",
+	"models/tnb/citizens/male_09.mdl",
+	"models/tnb/citizens/male_10.mdl",
+	"models/tnb/citizens/male_11.mdl",
+	"models/tnb/citizens/male_12.mdl",
+	"models/tnb/citizens/male_13.mdl",
+	"models/tnb/citizens/male_14.mdl",
+	"models/tnb/citizens/male_15.mdl",
+	"models/tnb/citizens/male_16.mdl",
+	"models/tnb/citizens/male_17.mdl",
+	"models/tnb/citizens/male_18.mdl"
+	}
 };
 
 -- Called when a player is transferred to the faction.
