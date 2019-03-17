2a49542f5fb2d2091c7e8d1bf7ec0f38a1f9ea19
The Lion <visiongamingjake@hotmail.com>
2019-03-17 13:07:13 +0100

    Adds Ministry Faction.

---
+++ garrysmod/gamemodes/cwhl2rp/plugins/ministry/plugin/classes/sh_minprop.lua
@@ -0,0 +1,13 @@
+--[[
+	© CloudSixteen.com do not share, re-distribute or modify
+	without permission of its author (kurozael@gmail.com).
+--]]
+
+local CLASS = Clockwork.class:New("Minister of Propaganda");
+	CLASS.color = Color(106, 156, 237, 255);
+	CLASS.wages = 25;
+	CLASS.factions = {FACTION_MINISTRY};
+	CLASS.wagesName = "Allowance";
+	CLASS.description = "A human employee of the Ministry.";
+	CLASS.defaultPhysDesc = "Wearing a neat suit.";
+CLASS_MINISTRYPROP = CLASS:Register();
---
+++ garrysmod/gamemodes/cwhl2rp/plugins/ministry/plugin/sh_plugin.lua
@@ -0,0 +1,9 @@
+--[[
+This plugin was initially created for Bitter Avalanche.
+
+Use is public, but these credits must not be removed at any date or time.
+--]]
+
+local PLUGIN = PLUGIN;
+
+Clockwork.kernel:IncludePrefixed("sv_hooks.lua");
